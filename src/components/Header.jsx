b209b12ef31168a72fd960cce3d2f829905c53a6
JC Soriano <jc@juancarlosoriano.ca>
2019-12-07 10:36:56 -0500

    Fix visual bug with extra padding

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,11 @@
 import React from "react";
 
 const Header = () => {
-  return <header className="c-header"></header>;
+  return (
+    <header className="c-header">
+      <h1 className="c-header__text">Keeper App</h1>
+    </header>
+  );
 };
 
 export default Header;
