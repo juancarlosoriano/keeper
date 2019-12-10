65ccb01549bab19b71e45497eac7e79aee11d723
JC Soriano <jc@juancarlosoriano.ca>
2019-12-10 14:33:56 -0500

    Updates

---
+++ precache-manifest.5d75e49d9e2eeb22ad7d8e5cbbd1cb44.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cefa58fbd62849fee8d63f971876a54b",
+    "url": "/keeper/index.html"
+  },
+  {
+    "revision": "bc970f36b673185ff4f0",
+    "url": "/keeper/static/css/main.e4f45dbe.chunk.css"
+  },
+  {
+    "revision": "536551c6365131981360",
+    "url": "/keeper/static/js/2.3c869cce.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/keeper/static/js/2.3c869cce.chunk.js.LICENSE"
+  },
+  {
+    "revision": "bc970f36b673185ff4f0",
+    "url": "/keeper/static/js/main.18e9eb6a.chunk.js"
+  },
+  {
+    "revision": "b33efabeec7a5d1dfba2",
+    "url": "/keeper/static/js/runtime-main.4b284dae.js"
+  }
+]);
